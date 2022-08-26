fb688ad6121a4eca40421b02b6fda88af84f0bf8
vfru <105499787+vfru@users.noreply.github.com>
2022-08-26 16:02:45 +0800

    Delete extension1 directory

--- extension1/src/components/2_lazyLoad/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Loading() {
-  return (
-    <div>
-      <h1 style={{backgroundColor:'gray',color:'orange'}} >Loading.......</h1>
-    </div>
-  )
-}
